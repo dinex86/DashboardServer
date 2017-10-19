cb7cb17e2f69d2085a6e5bd4f0d767846e875a4c
m.ewert <github@markus-ewert.de>
2017-10-19 22:38:49 +0200

    Added more information to PCars.

--- DashboardServer/Game/PCarsGame.cs
+++ DashboardServer/Game/PCarsGame.cs
@@ -29,46 +29,50 @@ namespace DashboardServer.Game
 
         protected override void UpdateExchangeData(pCarsAPIStruct shared)
         {
-            // ControlType 0 = Player.
-           /* bool isPlayer = shared.ControlType == 0;
-            bool isDriving = shared.GameInMenus != 1 && shared.GamePaused != 1;
+            pCarsAPIParticipantStruct player = shared.mParticipantData[shared.mViewedParticipantIndex];
 
+            // ControlType 0 = Player.
+            bool isPlayer = true;
+            bool isDriving = (int)shared.mPitMode != (int)ePitMode.PIT_MODE_IN_GARAGE;
+            bool inPit = (int)shared.mPitMode == (int)ePitMode.PIT_MODE_DRIVING_INTO_PITS || (int)shared.mPitMode == (int)ePitMode.PIT_MODE_IN_PIT || (int)shared.mPitMode == (int)ePitMode.PIT_MODE_DRIVING_OUT_OF_PITS;
+            
             // Add 5% to lap distance fraction to prevent from triggering while spinning or after a crash.
-            bool newLapStart = isPlayer && shared.LapDistanceFraction < data.LapDistanceFraction - 0.05 && isDriving && shared.LapTimeCurrentSelf > 0 && shared.LapTimeCurrentSelf < data.LapTimeCurrentSelf;
+            bool newLapStart = isPlayer && player.mCurrentLapDistance < player.mCurrentLapDistance - 0.05 && isDriving && shared.mCurrentTime > 0 && shared.mCurrentTime < data.LapTimeCurrentSelf;
 
             // Session info.
-            switch (shared.SessionLengthFormat)
+            /*switch ((int)shared.mRaceState)
             {
-                case 0:
-                    data.RaceFormat = (int)ExchangeData.RaceFormatIndex.LAP;
-                    break;
+                case eSessionState.:
+                data.RaceFormat = (int)ExchangeData.RaceFormatIndex.LAP;
+                break;
                 case 1:
-                    data.RaceFormat = (int)ExchangeData.RaceFormatIndex.TIME;
-                    break;
+                data.RaceFormat = (int)ExchangeData.RaceFormatIndex.TIME;
+                break;
                 case 2:
-                    data.RaceFormat = (int)ExchangeData.RaceFormatIndex.TIME_AND_EXTRA_LAP;
-                    break;
-            }
+                data.RaceFormat = (int)ExchangeData.RaceFormatIndex.TIME_AND_EXTRA_LAP;
+                break;
+            }*/
 
-            switch (shared.SessionType)
+            switch ((int)shared.mSessionState)
             {
-                case 0:
+                case (int)eSessionState.SESSION_PRACTICE:
+                case (int)eSessionState.SESSION_TEST:
+                case (int)eSessionState.SESSION_TIME_ATTACK:
                     data.Session = (int)ExchangeData.SessionIndex.PRACTICE;
                     break;
-                case 1:
+                case (int)eSessionState.SESSION_QUALIFY:
                     data.Session = (int)ExchangeData.SessionIndex.QUALIFY;
                     break;
-                case 2:
+                case (int)eSessionState.SESSION_FORMATIONLAP:
+                case (int)eSessionState.SESSION_RACE:
                     data.Session = (int)ExchangeData.SessionIndex.RACE;
                     break;
             }
 
-            data.SessionIteration = shared.SessionIteration;
-
-
+            data.SessionIteration = -1; // shared.SessionIteration;
+            
             if (newLapStart)
             {
-
                 Console.WriteLine("-- NEW LAP DETECTED --");
 
                 double currentLapTimeInMillis = Math.Round(shared.LapTimeCurrentSelf, 3) * 1000;
@@ -82,32 +86,32 @@ namespace DashboardServer.Game
 
                 data.LastLapStart = (long)currentLapStartTimeInMillis;
                 data.TriggerFuelCalculation();
-            }*/
+            }
 
             // General.
             data.CarSpeed = (int)Math.Round(shared.mSpeed);
             data.Gear = shared.mGear;
             data.MaxEngineRpm = (int)Math.Round(shared.mMaxRPM);
             data.EngineRpm = (int)Math.Round(shared.mRPM);
-            data.FuelLeft = shared.mFuelLevel;
+            data.FuelLeft = shared.mFuelLevel * shared.mFuelCapacity;
             data.NumberOfLaps = (shared.mLapsInEvent == 0) ? (int)shared.mLapsInEvent : -1;
-            /*data.CompletedLaps = shared.;
-            data.Position = shared.Position;
-            data.NumCars = shared.NumCars;
-            data.PitLimiter = shared.PitLimiter;
-            data.InPitLane = shared.InPitlane;
-            data.BreakBias = shared.BrakeBias;
-            data.LapDistanceFraction = shared.LapDistanceFraction;
+            data.CompletedLaps = (int)player.mLapsCompleted;
+            data.Position = (int)player.mRacePosition;
+            data.NumCars = shared.mNumParticipants;
+            data.PitLimiter = -1; // (int)shared.mCarFlags[(int)eCarFlags.CAR_SPEED_LIMITER];
+            data.InPitLane = inPit ? 1 : 0; // shared.mPitMode;
+            data.BreakBias = -1;// player.
+            data.LapDistanceFraction = player.mCurrentLapDistance / shared.mTrackLength;
             data.TractionControl = -1;
             data.Abs = -1;
-            */
+            
             /*
             // DRS.
-            data.DrsAvailable = shared.Drs.Available;
+            data.DrsAvailable = shared.drs
             data.DrsEngaged = shared.Drs.Engaged;
             data.DrsEquipped = shared.Drs.Equipped;
             data.DrsNumActivationsLeft = shared.Drs.NumActivationsLeft;
-
+            
             // P2P.
             data.PushToPassEquipped = shared.PushToPass.Available;
             data.PushToPassAvailable = shared.PushToPass.Available;
@@ -115,51 +119,61 @@ namespace DashboardServer.Game
             data.PushToPassNumActivationsLeft = shared.PushToPass.AmountLeft;
             data.PushToPassEngagedTimeLeft = shared.PushToPass.EngagedTimeLeft;
             data.PushToPassWaitTimeLeft = shared.PushToPass.WaitTimeLeft;
+            */
 
             // Temps.
-            data.OilTemperature = shared.EngineOilTemp;
-            data.WaterTemperature = shared.EngineWaterTemp;
+            data.OilTemperature = shared.mOilTempCelsius;
+            data.WaterTemperature = shared.mWaterTempCelsius;
+            data.TrackTemperature = shared.mTrackTemperature;
+            data.AirTemperature = shared.mAmbientTemperature;
 
             // Break temps.
-            data.BreakTempFrontLeft = Math.Round(shared.BrakeTemp.FrontLeft, 1);
-            data.BreakTempFrontRight = Math.Round(shared.BrakeTemp.FrontRight, 1);
-            data.BreakTempRearLeft = Math.Round(shared.BrakeTemp.RearLeft, 1);
-            data.BreakTempRearRight = Math.Round(shared.BrakeTemp.RearRight, 1);
-
+            data.BreakTempFrontLeft = Math.Round(shared.mBrakeTempCelsius[(int)eTyres.TYRE_FRONT_LEFT], 1);
+            data.BreakTempFrontRight = Math.Round(shared.mBrakeTempCelsius[(int)eTyres.TYRE_FRONT_RIGHT], 1);
+            data.BreakTempRearLeft = Math.Round(shared.mBrakeTempCelsius[(int)eTyres.TYRE_REAR_LEFT], 1);
+            data.BreakTempRearRight = Math.Round(shared.mBrakeTempCelsius[(int)eTyres.TYRE_REAR_RIGHT], 1);
+            
             // Damage.
+            /*
             data.DamageAerodynamics = shared.CarDamage.Aerodynamics;
             data.DamageEngine = shared.CarDamage.Engine;
             data.DamageTransmission = shared.CarDamage.Transmission;
+            */
 
             // Tire temps.
-            data.TireTempFrontLeft = Math.Round(shared.TireTemp.FrontLeft_Center, 1);
-            data.TireTempFrontRight = Math.Round(shared.TireTemp.FrontRight_Center, 1);
-            data.TireTempRearLeft = Math.Round(shared.TireTemp.RearLeft_Center, 1);
-            data.TireTempRearRight = Math.Round(shared.TireTemp.RearRight_Center, 1);
+            data.TireTempFrontLeft = Math.Round(shared.mTyreTemp[(int)eTyres.TYRE_FRONT_LEFT], 1);
+            data.TireTempFrontRight = Math.Round(shared.mTyreTemp[(int)eTyres.TYRE_FRONT_RIGHT], 1);
+            data.TireTempRearLeft = Math.Round(shared.mTyreTemp[(int)eTyres.TYRE_REAR_LEFT], 1);
+            data.TireTempRearRight = Math.Round(shared.mTyreTemp[(int)eTyres.TYRE_REAR_RIGHT], 1);
 
             // Tire wear.
-            data.TireWearFrontLeft = Math.Round(shared.TireWear.FrontLeft, 3);
-            data.TireWearFrontRight = Math.Round(shared.TireWear.FrontRight, 3);
-            data.TireWearRearLeft = Math.Round(shared.TireWear.RearLeft, 3);
-            data.TireWearRearRight = Math.Round(shared.TireWear.RearRight, 3);
+            data.TireWearFrontLeft = Math.Round(shared.mTyreWear[(int)eTyres.TYRE_FRONT_LEFT], 3);
+            data.TireWearFrontRight = Math.Round(shared.mTyreWear[(int)eTyres.TYRE_FRONT_RIGHT], 3);
+            data.TireWearRearLeft = Math.Round(shared.mTyreWear[(int)eTyres.TYRE_REAR_LEFT], 3);
+            data.TireWearRearRight = Math.Round(shared.mTyreWear[(int)eTyres.TYRE_REAR_RIGHT], 3);
 
             // Tire pressure.
-            data.TirePressureFrontLeft = Math.Round(ConvertPressureToPsi(shared.TirePressure.FrontLeft), 1);
-            data.TirePressureFrontRight = Math.Round(ConvertPressureToPsi(shared.TirePressure.FrontRight), 1);
-            data.TirePressureRearLeft = Math.Round(ConvertPressureToPsi(shared.TirePressure.RearLeft), 1);
-            data.TirePressureRearRight = Math.Round(ConvertPressureToPsi(shared.TirePressure.RearRight), 1);
+            /*
+            data.TirePressureFrontLeft = Math.Round(ConvertPressureToPsi(shared.tyre[(int)eTyres.TYRE_FRONT_LEFT]), 1);
+            data.TirePressureFrontRight = Math.Round(ConvertPressureToPsi(shared.TirePressure[(int)eTyres.TYRE_FRONT_RIGHT]), 1);
+            data.TirePressureRearLeft = Math.Round(ConvertPressureToPsi(shared.TirePressure[(int)eTyres.TYRE_REAR_LEFT]), 1);
+            data.TirePressureRearRight = Math.Round(ConvertPressureToPsi(shared.TirePressure[(int)eTyres.TYRE_REAR_RIGHT]), 1);
+            */
 
             // Tire dirt.
-            data.TireDirtFrontLeft = Math.Round(shared.TireDirt.FrontLeft, 1);
-            data.TireDirtFrontRight = Math.Round(shared.TireDirt.FrontRight, 1);
-            data.TireDirtRearLeft = Math.Round(shared.TireDirt.RearLeft, 1);
-            data.TireDirtRearRight = Math.Round(shared.TireDirt.RearRight, 1);
+            /*
+            data.TireDirtFrontLeft = Math.Round(shared.[(int)eTyres.TYRE_FRONT_LEFT], 1);
+            data.TireDirtFrontRight = Math.Round(shared.TireDirt[(int)eTyres.TYRE_FRONT_RIGHT], 1);
+            data.TireDirtRearLeft = Math.Round(shared.TireDirt[(int)eTyres.TYRE_REAR_LEFT], 1);
+            data.TireDirtRearRight = Math.Round(shared.TireDirt[(int)eTyres.TYRE_REAR_RIGHT], 1);
+            */
 
             // Timing.
-            data.CurrentLapValid = shared.CurrentLapValid;
-            data.LapTimeCurrentSelf = Math.Round(shared.CurrentLapValid > 0 ? shared.LapTimeCurrentSelf : (data.LastLapStart > 0 ? ((isDriving ? Now : data.LastTimeOnTrack) - data.LastLapStart) / 1000.0 : -1), 3);
-
-            if (shared.LapTimePreviousSelf > 0)
+            data.CurrentLapValid = shared.mLapInvalidated ? 0 : 1;
+            //data.LapTimeCurrentSelf = Math.Round(shared.CurrentLapValid > 0 ? shared.LapTimeCurrentSelf : (data.LastLapStart > 0 ? ((isDriving ? Now : data.LastTimeOnTrack) - data.LastLapStart) / 1000.0 : -1), 3);
+            data.LapTimeCurrentSelf = shared.mCurrentTime;
+            data.LapTimePreviousSelf = shared.mLastLapTime;
+            /*if (shared.LapTimePreviousSelf > 0)
             {
                 if (data.LastLapValid == 0)
                 {
@@ -167,99 +181,55 @@ namespace DashboardServer.Game
                     data.LastLapValid = 1;
                 }
                 data.LapTimePreviousSelf = Math.Round(shared.LapTimePreviousSelf, 3);
-            }
-
-            data.LapTimeBestSelf = Math.Round(shared.LapTimeBestSelf, 3);
-            data.LapTimeBestLeader = Math.Round(shared.LapTimeBestLeader, 3);
-            data.LapTimeBestLeaderClass = Math.Round(shared.LapTimeBestLeaderClass, 3);
-            data.SessionTimeRemaining = Math.Round(shared.SessionTimeRemaining, 3);
-            data.LapTimeDeltaLeader = Math.Round(shared.LapTimeDeltaLeader, 3);
-            data.LapTimeDeltaLeaderClass = Math.Round(shared.LapTimeDeltaLeaderClass, 3);
-
-
+            }*/
+            
+            data.LapTimeBestSelf = Math.Round(shared.mPersonalFastestLapTime, 3);
+            data.LapTimeBestLeader = Math.Round(shared.mSessionFastestLapTime, 3);
+            data.LapTimeBestLeaderClass = Math.Round(shared.mSessionFastestLapTime, 3);
+            data.SessionTimeRemaining = Math.Round(shared.mEventTimeRemaining, 3);
+            data.LapTimeDeltaLeader = -1;// Math.Round(shared.LapTimeDeltaLeader, 3);
+            data.LapTimeDeltaLeaderClass = -1;//Math.Round(shared.LapTimeDeltaLeaderClass, 3);
+            
             // Delta.
-            if (data.Session == (int)ExchangeData.SessionIndex.RACE)
-            {
-                data.TimeDeltaBehind = Math.Round(shared.TimeDeltaBehind, 3);
-                data.TimeDeltaFront = Math.Round(shared.TimeDeltaFront, 3);
-            }
-            else
-            {
-                // Get from leaderboard.
-                bool foundFront = false;
-                bool foundBehind = false;
-                for (int i = 0; i < shared.DriverData.Length; i++)
-                {
-                    DriverData otherDriver = shared.DriverData[i];
-                    if (!foundFront && otherDriver.Place == data.Position - 1)
-                    {
-                        data.TimeDeltaFront = CalcSectorDiff(shared.SectorTimesBestSelf, otherDriver.SectorTimeBestSelf);
-                        foundFront = true;
-                    }
+            data.TimeDeltaBehind = Math.Round(shared.mSplitTimeBehind, 3);
+            data.TimeDeltaFront = Math.Round(shared.mSplitTimeAhead, 3);
 
-                    if (!foundBehind && otherDriver.Place == data.Position + 1)
-                    {
-                        data.TimeDeltaBehind = CalcSectorDiff(otherDriver.SectorTimeBestSelf, shared.SectorTimesBestSelf);
-                        foundBehind = true;
-                    }
+            data.DeltaBestSelf = Math.Round(shared.mSplitTime, 3);
+            data.DeltaBestSession = -1;//CalcSectorDiff(shared.SectorTimesCurrentSelf, shared.SectorTimesPreviousSelf, shared.SectorTimesSessionBestLap);
 
-                    if (foundFront && foundBehind)
-                    {
-                        break;
-                    }
-                }
-
-                if (!foundFront)
-                {
-                    data.TimeDeltaFront = -1;
-                }
-
-                if (!foundBehind)
-                {
-                    data.TimeDeltaBehind = -1;
-                }
-            }
-
-            data.DeltaBestSelf = CalcSectorDiff(shared.SectorTimesCurrentSelf, shared.SectorTimesPreviousSelf, shared.SectorTimesBestSelf);
-            data.DeltaBestSession = CalcSectorDiff(shared.SectorTimesCurrentSelf, shared.SectorTimesPreviousSelf, shared.SectorTimesSessionBestLap);
-
-            data.CurrentSector = shared.TrackSector;
+            data.CurrentSector = (int)player.mCurrentSector;
 
             // Flags.
-            data.YellowFlagAhead = shared.ClosestYellowDistanceIntoTrack > 0 && shared.ClosestYellowDistanceIntoTrack < 1000;
-            data.YellowSector1 = shared.SectorYellow.Sector1;
-            data.YellowSector2 = shared.SectorYellow.Sector2;
-            data.YellowSector3 = shared.SectorYellow.Sector3;
-
-            if (shared.NumPenalties > 0 || shared.ExtendedFlags2.YellowPositionsGained > 0)
-            {
-                data.CurrentFlag = (int)ExchangeData.FlagIndex.PENALTY;
-            }
-            else if (shared.ExtendedFlags.Green == 1)
+            data.YellowFlagAhead = false;
+            data.YellowSector1 = -1;
+            data.YellowSector2 = -1;
+            data.YellowSector3 = -1;
+            
+            if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_GREEN)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.GREEN;
             }
-            else if (shared.Flags.Yellow == 1)
+            else if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_YELLOW || (int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_DOUBLE_YELLOW)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.YELLOW;
             }
-            else if (shared.ExtendedFlags2.White == 1)
+            else if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_WHITE)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.WHITE;
             }
-            else if (shared.Flags.Black == 1)
+            else if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_BLACK)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.BLACK;
             }
-            else if (shared.Flags.Blue == 1)
+            else if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_BLUE)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.BLUE;
             }
-            else if (shared.ExtendedFlags.Checkered == 1)
+            else if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_CHEQUERED)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.CHECKERED;
             }
-            else if (shared.ExtendedFlags.BlackAndWhite > 0)
+            else if ((int)shared.mHighestFlagColour == (int)eFlagColors.FLAG_COLOUR_WHITE)
             {
                 data.CurrentFlag = (int)ExchangeData.FlagIndex.BLACK_WHITE;
             }
@@ -269,6 +239,7 @@ namespace DashboardServer.Game
             }
 
             // Timestamps.
+            /*
             if (shared.InPitlane == 1 || shared.GameInMenus == 1)
             {
                 data.LastTimeInPit = Now;
